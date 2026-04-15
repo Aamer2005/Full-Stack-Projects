4ff78d2f924602c39a78ba04f06dcaa51a9bcf98
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-15 19:46:34 +0530

    Test Cases , request , models , environment

---
+++ part3-notes-backend-part4-2/asyncawait.js
@@ -0,0 +1,10 @@
+console.log("starts")
+
+
+// const promise = new Promise(
+//     (req,res)=>{
+   
+//         setTimeout((x=10,y=1)=>{console.log(x+y)},2000);
+//     }
+
+// );
--- part3-notes-backend-part4-2/index.js
+++ part3-notes-backend-part4-2/index.js
@@ -4,5 +4,5 @@ const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
-  logger.info(`http://localhost:3000/api/notes`)
+  logger.info(`http://localhost:${config.PORT}/api/notes`)
 })
